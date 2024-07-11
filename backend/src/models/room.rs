28a28e45f655331bf5d32adf4ddd607674d3ef4a
pcaracal <peter.schreivogel@stud.kbw.ch>
2024-07-11 08:32:14 +0200

    feat: room model & test data

---
+++ backend/src/models/room.rs
@@ -0,0 +1,55 @@
+use diesel::{
+    prelude::*, AsChangeset, ExpressionMethods, Insertable, QueryDsl, Queryable, RunQueryDsl,
+    Selectable,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::{schema::room, util::conn};
+
+#[derive(
+    Queryable, PartialEq, Selectable, Insertable, Serialize, Deserialize, Debug, AsChangeset,
+)]
+#[diesel(table_name = crate::schema::room)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Room {
+    pub id: Option<i32>,
+    pub name: String,
+}
+
+impl Room {
+    #[must_use]
+    pub fn create(name: &str) -> Option<Room> {
+        let new_room = Room {
+            id: None,
+            name: name.to_string(),
+        };
+
+        diesel::insert_into(room::table)
+            .values(&new_room)
+            .execute(&mut conn())
+            .ok()?;
+
+        room::table
+            .filter(room::name.eq(&name))
+            .order(room::id.desc())
+            .first(&mut conn())
+            .ok()
+    }
+
+    #[must_use]
+    pub fn all() -> Vec<Room> {
+        room::table.load(&mut conn()).unwrap_or_default()
+    }
+
+    #[must_use]
+    pub fn by_id(id: i32) -> Option<Room> {
+        room::table.filter(room::id.eq(id)).first(&mut conn()).ok()
+    }
+
+    #[must_use]
+    pub fn delete(id: i32) -> bool {
+        diesel::delete(room::table.filter(room::id.eq(id)))
+            .execute(&mut conn())
+            .is_ok()
+    }
+}
--- backend/src/util.rs
+++ backend/src/util.rs
@@ -4,7 +4,7 @@ use diesel::{Connection, SqliteConnection};
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 
-use crate::models::user::User;
+use crate::models::{room::Room, user::User};
 
 #[allow(clippy::missing_panics_doc)]
 pub fn setup_logger() {
@@ -26,13 +26,26 @@ pub fn conn() -> SqliteConnection {
 }
 
 pub fn load_test_data() {
-    info!("Deleting all users");
+    warn!("Deleting all data");
     info!(
         "{:?}",
         Command::new("diesel").args(["migration", "redo"]).output()
     );
     info!("Loading test users");
-    let _ = User::create_hash(true, "Admin", "Adminname", "admin", "admin");
-    let _ = User::create_hash(false, "User", "Username", "user", "user");
-    info!("Test users loaded {:#?}", User::all());
+    info!(
+        "{:?}",
+        User::create_hash(true, "Admin", "Adminname", "admin", "admin")
+    );
+    info!(
+        "{:?}",
+        User::create_hash(false, "User", "Username", "user", "user")
+    );
+    warn!("Test users loaded");
+
+    warn!("Loading test rooms");
+    info!("{:?}", Room::create("Room 1"));
+    info!("{:?}", Room::create("Room 2"));
+    info!("{:?}", Room::create("Room 3"));
+    info!("{:?}", Room::create("Room 4"));
+    warn!("Test rooms loaded");
 }
