7228494a839efbbbccb7c0e1fd2433101d552c9d
pcaracal <peter.schreivogel@stud.kbw.ch>
2024-07-11 11:30:42 +0200

    fix: login endpoint logic improvements & more useful response

--- backend/src/routes/login.rs
+++ backend/src/routes/login.rs
@@ -15,21 +15,41 @@ pub struct Login {
     pub password: String,
 }
 
+#[allow(clippy::module_name_repetitions)]
+#[derive(JsonSchema, Serialize, Deserialize, Debug)]
+pub struct LoginResponse {
+    pub user: User,
+    pub token: String,
+}
+
 #[openapi(tag = "Login")]
 #[post("/login", data = "<login>")]
 #[allow(clippy::missing_panics_doc)]
-pub fn post_login(login: Json<Login>) -> Result<String, Status> {
-    let users = User::all();
-    for u in users {
-        if u.email == login.email && auth::verify_password(&login.password, &u.password) {
-            match auth::encode_token(u.id.unwrap()) {
-                Some(token) => return Ok(token),
-                None => return Err(Status::InternalServerError),
-            }
-        }
-    }
+pub fn post_login(login: Json<Login>) -> Result<Json<LoginResponse>, Status> {
+    info!("Login attempt: {:?}", login.email);
 
-    Err(Status::Unauthorized)
+    match User::by_email(&login.email) {
+        Some(u) => {
+            if auth::verify_password(&login.password, &u.password) {
+                match auth::encode_token(u.id.unwrap()) {
+                    Some(token) => {
+                        let response = LoginResponse {
+                            user: u,
+                            token: token.clone(),
+                        };
+
+                        info!("User logged in: {response:?}");
+
+                        return Ok(Json(response));
+                    }
+                    None => return Err(Status::InternalServerError),
+                }
+            }
+
+            Err(Status::Unauthorized)
+        }
+        _ => Err(Status::Unauthorized),
+    }
 }
 
 #[derive(JsonSchema, Serialize, Deserialize, Debug)]
