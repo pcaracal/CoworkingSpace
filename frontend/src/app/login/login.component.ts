6aa66dd47ea329bd3368a647908fa2279e61e612
pcaracal <peter.schreivogel@stud.kbw.ch>
2024-07-12 09:26:45 +0200

    feat: ui for login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  isRegister = false;
+
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() { }
+
+  onLogin(event: Event) {
+    event.preventDefault();
+    console.log("onLogin()", this.email, this.password);
+  }
+
+  onRegister(event: Event) {
+    event.preventDefault();
+    console.log("onRegister()", this.firstName, this.lastName, this.email, this.password);
+  }
 
 }
